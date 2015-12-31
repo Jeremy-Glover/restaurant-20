9a373055bcae4fcc4acf3fec3719cd784919e31d
Jeremy Glover <jeremyglover33@gmail.com>
2015-12-31 12:37:36 -0600

    Added admin routes, navbar styling

--- app/admin/menu-items/edit/route.js
+++ app/admin/menu-items/edit/route.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    saveItem(formData) {
+
+      let menuItem = this.modelFor('admin.menu-items.edit');
+
+      menuItem.setProperties(formData);
+
+      menuItem.save().then(() => {
+        this.transitionTo('admin.menu-items');
+      });
+    },
+  },
 });
--- app/admin/menu-items/new/route.js
+++ app/admin/menu-items/new/route.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+   addItem(formData) {
+
+     let menuItem = this.store.createRecord('menu-item');
+
+     menuItem.setProperties(formData);
+
+     menuItem.save().then(() => {
+       this.transitionTo('admin.menu-items');
+     });
+   },
+ },
 });
